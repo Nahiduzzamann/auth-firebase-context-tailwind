c810d9b90592a03e8957322c2ed8f6080b95e18f
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2023-04-16 21:57:08 +0100

    setup

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            headrt
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD0Kv2f-aeCb0TvRYAbokqwu7XVhw_a95E",
+  authDomain: "auth-firebase-context-ta-d2736.firebaseapp.com",
+  projectId: "auth-firebase-context-ta-d2736",
+  storageBucket: "auth-firebase-context-ta-d2736.appspot.com",
+  messagingSenderId: "658334333345",
+  appId: "1:658334333345:web:cc62d052df3fc8af42f3e0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
